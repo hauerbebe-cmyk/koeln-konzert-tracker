b5fe3b35e64b8e19ba2ea543b8bc154dd050d6f9
hauerbebe-cmyk <hauerbebe@gmail.com>
2026-09-22 17:23:30 +0200

    Create gloria_theater.py

---
+++ sources/venues/gloria_theater.py
@@ -0,0 +1,14 @@
+"""Venue-Scraper: Gloria Theater Köln.
+
+TODO: Noch nicht implementiert -- gleiches Vorgehen wie bei palladium.py
+(zuerst JSON-Endpoint suchen) bzw. live_music_hall.py (sonst HTML-Scraping).
+"""
+
+from __future__ import annotations
+
+from ..base import Event
+
+
+def fetch() -> list[Event]:
+    # TODO: echten Endpoint/HTML-Parsing für gloria-theater.de ergänzen.
+    return []
